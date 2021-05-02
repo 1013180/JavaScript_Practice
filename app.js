fcb5dc4ef2ccded8d3a935fe24572a0f86b3a895
1013180 <hiroki.kojima0417@gmail.com>
2021-05-02 09:29:29 +0900

    Start,Chapter3

---
+++ app.js
@@ -0,0 +1,14 @@
+var hello = "Hello World";
+alert('Hello' + 'World');
+
+
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
