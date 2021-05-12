be224decd80fac25750c4ff96beede6c55fc7f05
1013180 <hiroki.kojima0417@gmail.com>
2021-05-12 14:42:37 +0900

    Add,SlideShow

--- Pagetop-Lesson/js/script.js
+++ Pagetop-Lesson/js/script.js
@@ -14,3 +14,17 @@ $(function() {
     event.preventDefault();
   });
 });
+
+
+$(".theTarget").skippr({
+  transition : 'fade',
+  speed : 1000,
+  easing : 'easeOutQuart',
+  navType : 'block',
+  childrenElementType : 'div',
+  arrows : true,
+  autoPlay : true,
+  autoPlayDuration : 2000,
+  keyboardOnAlways : false,
+  hidePrevious : false
+});
